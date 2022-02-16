09a9cfbb0ff5673614e1d78f06a8f1fa5fcf8a85
Dequé <carolinedeque@gmail.com>
2022-02-16 17:05:43 +0100

    Exercice 2.1

--- Utilisez-des-design-patterns-en-JavaScript/js/App.js
+++ Utilisez-des-design-patterns-en-JavaScript/js/App.js
@@ -1,19 +1,17 @@
 class App {
   constructor() {
     this.$moviesWrapper = document.querySelector(".movies-wrapper");
-    this.moviesApi = new MovieApi("./data/old-movie-data.json");
+    this.moviesApi = new MovieApi("./data/new-movie-data.json");
   }
 
   async main() {
+    // Ici je récupère mes films de mon fichier old-movie-data.json
     const moviesData = await this.moviesApi.getMovies();
 
     moviesData
-      .map((movie) => new OldMovie(movie))
+      // Ici, je transforme mon tableau de données en un tableau de classe Movie
+      .map((movie) => new Movie(movie))
       .forEach((movie) => {
-        console.log("===");
-        console.log(movie);
-        console.log("===");
-
         const Template = new MovieCard(movie);
         this.$moviesWrapper.appendChild(Template.createMovieCard());
       });
